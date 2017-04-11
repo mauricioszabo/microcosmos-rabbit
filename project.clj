7692c74fbf86b5f600cb65416f109f7245234287
Maurício Szabo <mauricio.szabo@gmail.com>
2017-04-11 17:32:38 -0300

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject microscope/rabbit "0.1.0"
+(defproject microscope/rabbit "0.1.1"
   :description "RabbitMQ implementation for Microscope"
   :url "https://github.com/acessocard/microscope"
   :license {:name "Eclipse Public License"
