57f2445f270f710d9baa96c78a6fbc3852db6306
Maurício Szabo <mauricio.szabo@gmail.com>
2017-04-13 18:37:10 -0300

    Mocked versions of RPC

--- test/microscope/rabbit/rpc_test.clj
+++ test/microscope/rabbit/rpc_test.clj
@@ -24,3 +24,31 @@
       (rpc 20) => 21))
   (background
    (after :facts (rabbit/disconnect!))))
+
+(facts "when mocking server"
+  (components/mocked
+    (let [subs (components/subscribe-with :rpc-server (rpc/queue "rpc"))]
+      (subs :rpc-server (fn [f-val {:keys [rpc-server]}]
+                          (->> f-val
+                               (future/map :payload)
+                               (future/intercept #(io/send! rpc-server
+                                                            {:payload (inc %)})))))
+
+      (fact "will publish to a mocked response queue"
+        (rpc/mock-call :rpc 10)
+        (-> @rpc/queues :rpc-response :messages deref last :payload) => 11))))
+
+(fact "when mocking client"
+  (components/mocked
+    {:rpc-responses {:rpc #(+ 2 %)}}
+    (let [subs (components/subscribe-with :caller (rpc/caller "rpc")
+                                          :queue (rabbit/queue "some-queue")
+                                          :response (rabbit/queue "response"))]
+      (subs :queue (fn [f-value {:keys [response caller]}]
+                     (->> f-value
+                          (future/map #(caller (:payload %)))
+                          (future/intercept #(io/send! response {:payload %})))))
+
+      (fact "calls mocked remove function"
+        (io/send! (:some-queue @rabbit/queues) {:payload 90}))
+      (-> @rabbit/queues :response :messages deref last :payload) => 92)))
