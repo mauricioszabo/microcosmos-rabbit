d1445cacc6640ed2713a838b9bc53ca5f2b5dfe2
Vinicius Higa <viniciushiga@gmail.com>
2017-04-28 11:10:30 -0300

    Upgrade to langohr 3.7.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.novemberain/langohr "3.6.1"]
+                 [com.novemberain/langohr "3.7.0"]
                  [microscope "0.1.3"]]
 
   :profiles {:dev {:src-paths ["dev"]
