efbeeca734eb74d1ba34ad66dd58f3fab29b14c3
Lenon Marcel <lenon.marcel@gmail.com>
2017-05-25 17:19:26 -0300

    Bump up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject microscope/rabbit "0.1.2"
+(defproject microscope/rabbit "0.1.3"
   :description "RabbitMQ implementation for Microscope"
   :url "https://github.com/acessocard/microscope"
   :license {:name "Eclipse Public License"
